b4f00eb05926988827bcf435c1425a374564b5fd
JodeCode <nwizugbejohn@gmail.com>
2025-02-07 18:45:14 +0100

    more addition

--- tasks/settings.py
+++ tasks/settings.py
@@ -45,6 +45,10 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,13 +124,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = '/media/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 
 #STATICFILES_DIRS =[BASE_DIR/'static']
 
-
+MEDIA_URL = '/media/'  
 MEDIA_ROOT = BASE_DIR/'static/media'
+
+
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
