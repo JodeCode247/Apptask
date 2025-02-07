d69dfbeb743b3fc7ac4404293654fc5d8112048c
JodeCode <nwizugbejohn@gmail.com>
2025-02-07 21:50:34 +0100

    j

--- tasks/settings.py
+++ tasks/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path 
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,11 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 
 
-STATICFILES_DIRS =[BASE_DIR/'static']
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'  
 MEDIA_ROOT = BASE_DIR/'static/media'
