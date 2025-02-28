d2c3f3c2b41cdb21f9c2b716151a4c55dee584a7
JodeCode <nwizugbejohn@gmail.com>
2025-02-28 15:48:18 +0100

    SEtting database included

--- tasks/settings.py
+++ tasks/settings.py
@@ -80,8 +80,8 @@ if DEBUG:
 
 if not DEBUG:
     if not DEBUG:
-    DATABASES = {
-     'default': dj_database_url.parse('postgresql://postgres:GYHrgGnEPVGFIhGfREsMXHxukAtISxFg@yamanote.proxy.rlwy.net:35852/railway',
+        DATABASES = {
+       'default': dj_database_url.parse('postgresql://postgres:GYHrgGnEPVGFIhGfREsMXHxukAtISxFg@yamanote.proxy.rlwy.net:35852/railway',
         conn_max_age=600,)
  }
     
